c49aca9e4763b3f3da1878f0af4abfcb0ea5e5d5
HoubaKing <145403532+HoubaKing@users.noreply.github.com>
2023-09-19 08:32:26 -0400

    Update Streamlit_App.py
    Adding Header & Text

--- Streamlit_App.py
+++ Streamlit_App.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title('New Parents Dinner')
+streamlit.header('BreakFast Menu')
+streamlit.text('Omega 3 & Blueberry')
+streamlit.text('Spinach & Rocket Smoothie')
+streamlit.text('Range Egg')
 
